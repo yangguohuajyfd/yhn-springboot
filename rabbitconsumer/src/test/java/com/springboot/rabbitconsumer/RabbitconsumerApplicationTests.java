c9f620b8db5adfdb865f7ba8decc0580e6525c22
yanghongnian <conan0220>
2022-02-28 10:25:08 +0800

    diyici

---
+++ rabbitconsumer/src/test/java/com/springboot/rabbitconsumer/RabbitconsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.rabbitconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitconsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
