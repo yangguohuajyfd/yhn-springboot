9d62905161cadfa295c18879f2e11fb377b4b775
yanghongnian <conan0220>
2022-03-15 10:12:07 +0800

    diyici

---
+++ elasticsearch/src/main/java/com/yhn/elasticsearch/config/ElasticSearchConfig.java
@@ -0,0 +1,29 @@
+package com.yhn.elasticsearch.config;
+
+
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestClientBuilder;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ElasticSearchConfig {
+
+
+    private static final RequestOptions REQUEST_OPTIONS;
+
+    static {
+        RequestOptions.Builder builder = RequestOptions.DEFAULT.toBuilder();
+        REQUEST_OPTIONS = builder.build();
+
+    }
+
+    @Bean
+    public RestHighLevelClient esConfig(){
+        RestHighLevelClient restHighLevelClient = new RestHighLevelClient(RestClient.builder(new HttpHost("39.106.81.231",9200 , "http")));
+        return restHighLevelClient;
+    }
+}
--- elasticsearch/src/test/java/com/yhn/elasticsearch/ElasticsearchApplicationTests.java
+++ elasticsearch/src/test/java/com/yhn/elasticsearch/ElasticsearchApplicationTests.java
@@ -1,13 +1,47 @@
 package com.yhn.elasticsearch;
 
+import com.alibaba.fastjson.JSON;
+import lombok.Data;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.index.IndexRequestBuilder;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.xcontent.XContentType;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.IOException;
 
 @SpringBootTest
 class ElasticsearchApplicationTests {
 
+    @Autowired
+    private RestHighLevelClient client;
+
     @Test
-    void contextLoads() {
+    void contextLoads() throws IOException {
+        IndexRequest user = new IndexRequest("user");
+
+
+
+        user.id();
+        User user1 = new User();
+        user1.setAge(18);
+        user1.setName("yanghongnian");
+        user1.setGender("n");
+        String s = JSON.toJSONString(user1);
+         user.source(s, XContentType.JSON);
+
+        client.index(user, RequestOptions.DEFAULT);
+
+    }
+
+    @Data
+    class User{
+        private String name ;
+        private Integer age ;
+        private String gender ;
     }
 
 }
