a60bc23cf4b30639e6ef7e573e07f4921ba817f5
yanghongnian <conan0220>
2022-04-06 14:46:53 +0800

    diyici

---
+++ demo/src/main/java/com/example/demo/StudyHashmap.java
@@ -0,0 +1,32 @@
+package com.example.demo;
+
+import java.util.HashMap;
+
+public class StudyHashmap {
+
+
+    public static void main(String[] args) {
+        HashMap hashMap = new HashMap();
+
+        hashMap.put("1",1);
+        hashMap.put("2",2);
+        hashMap.put("3",2);
+        hashMap.put("4",2);
+        hashMap.put("5",2);
+        hashMap.put("6",2);
+        hashMap.put("7",2);
+        hashMap.put("8",2);
+        hashMap.put("9",2);
+        hashMap.put("10",2);
+        hashMap.put("21",2);
+        hashMap.put("22",2);
+        hashMap.put("23",2);
+        hashMap.put("24",2);
+        hashMap.put("25",2);
+        System.out.println("123");
+
+    }
+
+
+
+}
